fbfe9aa63ecacef723deb1c501a7dc7ce78b1fb0
Carl Noval <cjrnoval22@gmail.com>
2022-01-10 23:43:15 +0900

    lecture finished, added delete verb destroy action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   # no only means will run :find_restaurant for both new and create
-  before_action :find_restaurant
+  before_action :find_restaurant, except: [ :destroy ]
 
   def new
     @review = Review.new
@@ -9,15 +9,39 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(strong_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      # render means render a page, for this case we're rendering the new page
+
+      # what happens:
+      # we're going to stay in the create action but display the template of the new page
+      # the only diffirence is that
+      #   - the instance of review is no longer the one from the #new method
+      #   - the instance of reveiew being used is the one that got failed to save
+
+      # rails c -> new_review = Review.new -> new_review.restaurant = Restaurant.all.sample -> new_review.save # => false
+      # new_review.errors.message -> new_review.errors.messages           # => {:content=>["can't be blank"]}
+      # in the UI creating a review with empty valie shows error message  # => Content can't be blank
+      # simple_form_for takes care of the message from previous line and making the box red
+      render :new
+    end
   end
 
+  def destroy
+    # no need for @ since this review instance is not passed to the view
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to restaurant_path(review.restaurant)
+  end
   private
 
   def find_restaurant
-    # no longer params[:id] since route is now different
-    # route is now: /restaurants/:restaurant_id/reviews/new
+    # params[:restaurant_id] instead of params[:id] to differentiate which id to use
+    # params[:restaurant_id] came from:
+    #                 Prefix Verb   URI Pattern                     Controller#Action
+    #     restaurant_reviews POST   /restaurants/:restaurant_id/reviews(.:format)     reviews#create
+    #  new_restaurant_review GET    /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :restaurants do
+    # collections and member routes are only for the properties of the model
+
     # collection url-path   - /restaurants/...    - /restaurants/top
     # member url-path       -/restaurant/:id/...  - /restaurants/:id/chef
     collection do
       # top is an arbitrary value
       #           Prefix Verb   URI Pattern                     Controller#Action
       #   top_restaurants GET    /restaurants/top(.:format)      restaurants#top
+      # :top could also be written as a string
       get :top
       # get :top could also be written as per below but rails is here to make life easier
       # get 'top', to: 'restaurants#top', as: :top_restaurants
@@ -19,6 +22,7 @@ Rails.application.routes.draw do
       # chef is an arbitrary value
       #          Prefix Verb   URI Pattern                                       Controller#Action
       # chef_restaurant GET    /restaurants/:id/chef(.:format)                   restaurants#chef
+      # :chef could also be written as a string
       get :chef
 
       # ditional routes may also be added
@@ -29,7 +33,12 @@ Rails.application.routes.draw do
     #     restaurant_reviews POST   /restaurants/:restaurant_id/reviews(.:format)     reviews#create
     #  new_restaurant_review GET    /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
     resources :reviews, only: [ :new, :create ]
-  end 
+  end
+
+  # not nested cause does not need the id of the restaurant
+  # Prefix Verb   URI Pattern                                       Controller#Action
+  # review DELETE /reviews/:id(.:format)                            reviews#destroy
+  resources :reviews, only: [ :destroy ]
 
   # For details on the DSL available within this file see https://guides.rubyonrails.org/routing.html
 end
